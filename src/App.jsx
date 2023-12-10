6f6926a0ee989c1295f6e6a212bc1c288ce22db2
Sarang Tripathi <sarangtripathi18@gmail.com>
2023-12-10 19:28:02 +0530

    pages and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
